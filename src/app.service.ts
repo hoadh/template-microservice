595095c0de94e78662e1df1f34b1d176b11e5f4c
hoadh <danghuyhoa@gmail.com>
2022-08-20 17:54:10 +0700

    Demo service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit, OnModuleDestroy {
+  constructor(@Inject("kafka-client") private readonly client: ClientKafka) { }
+
+  async onModuleInit() {
+    [
+      'NEW_ORDER',
+      'TAKE_ORDER',
+      'COMPLETE_ORDER'
+    ].forEach((key) => this.client.subscribeToResponseOf(`${key}`));
+    await this.client.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.client.close();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
+
 }
