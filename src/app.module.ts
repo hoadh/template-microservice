947c451e8b575f0423a06fa0af0c59ea4138fa02
hoadh <danghuyhoa@gmail.com>
2022-08-20 17:56:59 +0700

    Config kafka connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.register([{
+      name: 'kafka-client',
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'template-service',
+          brokers: ['localhost: 9091', 'localhost: 9092', 'localhost: 9093']
+        },
+        consumer: {
+          groupId: 'template-consumer-group',
+        }
+      }
+    }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
